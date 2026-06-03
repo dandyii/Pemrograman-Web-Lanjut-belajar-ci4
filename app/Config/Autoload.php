d3f748f7b851ebc4f5f37d965a78485cedd34730
dandyii <dandynugroho901@gmail.com>
2026-06-03 13:50:30 +0700

    Capstone 9 Library

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -39,6 +39,7 @@ class Autoload extends AutoloadConfig
      */
     public $psr4 = [
         APP_NAMESPACE => APPPATH,
+        'CodeIgniterCart' => ROOTPATH . 'vendor/jason-napolitano/codeigniter4-cart-module/src'
     ];
 
     /**
@@ -88,5 +89,5 @@ class Autoload extends AutoloadConfig
      *
      * @var list<string>
      */
-    public $helpers = ['url'];
+    public $helpers = ['url', 'form'];
 }
